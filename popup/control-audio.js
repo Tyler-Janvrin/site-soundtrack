e76a306337631ca150e730e5cca7fc3a0674c073
Tyler Janvrin <janvrin.tyler@gmail.com>
2024-09-09 15:10:14 -0400

    moved some things around to make it cleaner and set up messaging between background and popup

--- popup/control-audio.js
+++ popup/control-audio.js
@@ -1,3 +1,24 @@
+console.log("Hello from control-audio!");
+
+// code from https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/runtime/sendMessage
+
+function handleResponse(message) {
+    console.log(`Message from the background script: ${message.response}`);
+}
+  
+function handleError(error) {
+    console.log(`Error: ${error}`);
+}
+  
+function notifyBackgroundPage(e) {
+    const sending = browser.runtime.sendMessage({
+        greeting: "Greeting from the content script",
+    });
+    sending.then(handleResponse, handleError);
+}
+  
+document.getElementById("notify-button").addEventListener("click", notifyBackgroundPage);
+
 document.getElementById("play-button").addEventListener("click", (e) => {
     var secret_text = document.getElementById('hidden-text');
     secret_text.style.visibility = 'visible';
